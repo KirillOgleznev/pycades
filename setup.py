f3f654673c31ad29381ca60c6faaddf383f7521a
Ogleznev Kirill <ogleznevki@cifra-k.ru>
2023-06-30 00:26:49 +0700

    documentation: Добавил документацию на PyPi

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
+from pathlib import Path
 
 
 setup(
     name='pycades',
-    version='1.1.0',
+    version='1.1.2',
     author="Kirill",
     author_email="kirill24680@gmail.com",
     description="""Расширение Pycades предоставляет программный интерфейс, аналогичный КриптоПро""",
@@ -11,4 +12,6 @@ setup(
     package_data={
         'pycades': ['pycades.so'],
     },
+    long_description=Path("README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
 )
