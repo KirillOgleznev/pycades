467137af3299ff8a7c4f79e51f2db1234f0b634c
Ogleznev Kirill <ogleznevki@cifra-k.ru>
2023-06-30 11:50:35 +0700

    feature: pyproject.toml добавил сборку колес с зависимостями

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 from setuptools import setup
+from wheel.bdist_wheel import bdist_wheel
 from pathlib import Path
+import os
+
+
+class BdistWheelCommand(bdist_wheel):
+    def run(self):
+        """Run command."""
+        bdist_wheel.run(self)
+        auditwheel_cmd = 'auditwheel repair --plat {plat} dist/*.whl'
+        mv_cmd = 'mv ./wheelhouse/*.whl ./dist'
+        os.system(auditwheel_cmd.format(plat='manylinux_2_35_x86_64'))
+        os.system(auditwheel_cmd.format(plat='mv_cmd'))
 
 
 setup(
@@ -14,4 +26,7 @@ setup(
     },
     long_description=Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
+    cmdclass={
+        'bdist_wheel': BdistWheelCommand,
+    },
 )
